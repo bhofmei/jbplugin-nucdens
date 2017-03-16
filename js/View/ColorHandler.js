6791cc3a35afd71492674651f82dd87c3c020f76
Brigitte Hofmeister <bhofmei@gmail.com>
2017-03-16 14:35:53 -0400

    using chroma to do color conversions

--- js/View/ColorHandler.js
+++ js/View/ColorHandler.js
@@ -2,13 +2,13 @@ define('NucleotideDensityPlugin/View/ColorHandler',[
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
-    'dojo/_base/Color',
+    './chroma'
 ],
 function(
     declare,
     array,
     lang,
-    dojoColor
+     chroma
 
 ){
     /*
@@ -30,6 +30,7 @@ ColorHandler = {
     },
 
     generateNColors: function(n){
+        var thisB = this;
         var s=100, l=65, sep=360/n;
         var hs=[];
         var i;
@@ -37,16 +38,18 @@ ColorHandler = {
             hs.push(sep*i);
         }
         var out = array.map(hs, function(h){
-            var t = dojoxColor.fromHsl(h, s, l);
-            return t.toHex();
+            var t = chroma.hcl(h,s,l);
+            return t.hex();
         });
         return out;
     },
 
     getFontColor: function(color){
         // from http://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
-        var bg = new dojoColor(color);
-        var rgb = bg.toRgb();
+        //var bg = new dojoColor(color);
+        var bg = chroma(color);
+        //var rgb = bg.toRgb();
+        var rgb = bg.rgb();
         var a = 1 - ( 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2])/255;
         if(a >= 0.5)
             return '#F0F0F0';
@@ -96,7 +99,6 @@ ColorHandler = {
         n %= colorList.length;
         return colorList[n];
     }
-
     }
 return ColorHandler;
 });
